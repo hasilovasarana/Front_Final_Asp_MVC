c7d1fbd0eab69d8054bbc97ae2bf9f356effec67
hasilovasarana <saranakhh@code.edu.az>
2018-02-01 15:05:18 +0400

    front final asp net

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Front_Final_ASP_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Front_Final_ASP_MVC.MvcApplication" Language="C#" %>
